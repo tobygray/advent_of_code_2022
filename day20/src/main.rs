efe2594824b5589e6c15e4e2591a1a01c641bbd1
Toby Gray <toby.gray@gmail.com>
2023-04-16 17:09:39 +0100

    Day 20 part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,26 +1,28 @@
 use std::io::{self, BufRead};
 
 fn main() -> eyre::Result<()> {
-    let _sum = 0;
-    let mut numbers: Vec<(i32, usize)> = vec![];
+    let mut numbers: Vec<(i64, usize)> = vec![];
     for (idx, line) in io::stdin().lock().lines().enumerate() {
         let line = line?;
-        numbers.push((line.parse()?, idx));
+        numbers.push((line.parse::<i64>()? * 811589153, idx));
     }
     let count = numbers.len();
-    for original_idx in 0..count {
-        let current_pos = numbers.iter().position(|&x| x.1 == original_idx).unwrap();
-        let value = numbers[current_pos].0;
-        if value == 0 {
-            continue;
+    for _ in 0..10 {
+        for original_idx in 0..count {
+            let current_pos = numbers.iter().position(|&x| x.1 == original_idx).unwrap();
+            let value = numbers[current_pos].0;
+            if value == 0 {
+                continue;
+            }
+            numbers.remove(current_pos);
+            let count = numbers.len() as i64;
+            let mut new_pos_signed = current_pos as i64 + value;
+            while new_pos_signed < 0 {
+                new_pos_signed += (((-new_pos_signed) + count) / count) * count;
+            }
+            let new_pos = (new_pos_signed) as usize % (numbers.len());
+            numbers.insert(new_pos, (value, original_idx));
         }
-        numbers.remove(current_pos);
-        let mut new_pos_signed = current_pos as i32 + value;
-        while new_pos_signed < 0 {
-            new_pos_signed += numbers.len() as i32;
-        }
-        let new_pos = (new_pos_signed) as usize % (numbers.len());
-        numbers.insert(new_pos, (value, original_idx));
     }
     let zero_idx = numbers.iter().position(|&x| x.0 == 0).unwrap();
     let idx1 = (zero_idx + 1000) % numbers.len();
